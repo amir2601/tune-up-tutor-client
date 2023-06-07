99c6e3cdc8e5c7ebcdda1231e7eeb67c88047c2e
amir260101~ <amir26hossin@gmail.com>
2023-06-07 22:09:32 +0600

    footer done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Navbar from '../components/Shared/NavBar/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <div>
+            <div className='my-10'>
                 <Outlet></Outlet>
             </div>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Pages/Home/Home/PopularInstructorCard.jsx
+++ src/Pages/Home/Home/PopularInstructorCard.jsx
@@ -3,15 +3,8 @@ import React from 'react';
 const PopularInstructorCard = ({instructor}) => {
     return (
         <div>
-            <div className="card card-compact w-full bg-base-100 shadow-xl">
+            <div className="card card-compact w-full bg-base-100 shadow-xl hover:scale-95">
                 <figure><img src={instructor.img} alt="Shoes" /></figure>
-                {/* <div className="card-body">
-                    <h2 className="card-title">{instructor.name}</h2>
-                    <p>{instructor.email}</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div> */}
             </div>
         </div>
     );
